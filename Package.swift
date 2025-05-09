02ce8e493ed8d79de1d3ba03e50a709ef299dfa7
Liam Butler-Lawrence <liamdunn@icloud.com>
2025-05-09 11:59:30 -0700

    fix: use swift-collections fork

--- Package.swift
+++ Package.swift
@@ -595,7 +595,7 @@ let package = Package(
 if Context.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
     package.dependencies += [
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.1.0"),
+        .package(url: "https://github.com/candlefinance/swift-collections.git", exact: "1.1.4-candle"),
         .package(url: "https://github.com/apple/swift-system.git", from: "1.4.0"),
     ]
 } else {
